3fcf27c7c1375f89761a2dab10b1c81f5c60c8a0
Elkana Lumbantobing <elkana.ltobing@gmail.com>
2018-09-23 13:51:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BlogList from './components/BlogList'
+// import articles from './data/news.json'
+import axios from 'axios'
+
+// const link = "https://raw.githubusercontent.com/jakarta-workshop-react/react-blog/master/src/data/news.json"
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: []
+    }
+  }
+
+  // componentDidMount() {
+  //   fetch(link)
+  //     .then((data) => data.json())
+  //     .then((data) => {
+  //       this.setState({
+  //         articles: data
+  //       })
+  //     })
+  // }
+
+  componentDidMount() {
+    axios.get('https://raw.githubusercontent.com/jakarta-workshop-react/react-blog/master/src/data/news.json')
+      .then(res => {
+        this.setState({
+          articles: res.data
+        })
+      })
+  }
+
   render() {
+    // console.log(articles)
     return (
       <div className="App">
         <header className="App-header">
@@ -14,10 +46,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <BlogList />
-        <BlogList />
-        <BlogList />
-        <BlogList />
+
+        {this.state.articles.map((item, index) => <BlogList article={item} key={index} />)}
+
       </div>
     );
   }
